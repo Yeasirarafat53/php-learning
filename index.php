894adc4c0f687321f382641066ff4f3d4a14463d
Yeasirarafat53 <yeasirarafat1776@gmail.com>
2022-06-25 18:50:46 +0600

    DONE

--- index.php
+++ index.php
@@ -100,7 +100,25 @@ var_dump ($null);
 // Object
 // Resource
 
+// ==============magic constant============
+echo __FILE__ ;
+echo '<br>';
+echo __LINE__ ;
+echo '<br>';
+echo __DIR__ ;
+echo '<br>';
+echo __FUNCTION__ ;
+echo '<br>';
+echo __CLASS__ ;
+echo '<br>';
+echo __TRAIT__ ;
+echo '<br>';
+echo __METHOD__ ;
+echo '<br>';
 
+
+
+echo PHP_VERSION ;
 
 
 
